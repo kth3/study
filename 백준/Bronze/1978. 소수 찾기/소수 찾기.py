6d70ca8a2cd523fbf45ee82f14d5a9653f1857ba
kth <252437414+kth3@users.noreply.github.com>
2026-04-08 23:59:43 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(sys.stdin.readline())
+numbers = list(map(int, sys.stdin.readline().split()))
+
+prime_count = 0
+
+for num in numbers:
+    if num < 2:
+        continue
+    
+    is_prime = True
+
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+            
+    if is_prime:
+        prime_count += 1
+
+print(prime_count)
